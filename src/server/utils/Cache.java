706f24aaab47a79c5f6a731d0bfdc2c51bbd0ab6
JadirJunior <jadirjunior8@gmail.com>
2024-11-16 17:57:27 +0000

    refactor: creating users and products lists in a different class

---
+++ src/server/utils/Cache.java
@@ -0,0 +1,13 @@
+package server.utils;
+
+import entitites.Product;
+import entitites.User;
+
+import java.util.ArrayList;
+
+public class Cache {
+
+    public static ArrayList<User> usersList = new ArrayList<User>();
+    public static ArrayList<Product> productsList = new ArrayList<Product>();
+
+}
